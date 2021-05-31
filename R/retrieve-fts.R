92686c9d2548063ef4f2833d9557eb145c603d1d
Heidi Hunter <hunter@unhcr.org>
2021-05-31 13:19:31 -0400

    retrieve flows recursively

--- R/retrieve-fts.R
+++ R/retrieve-fts.R
@@ -1,14 +1,72 @@
-pacman::p_load(httr)
+pacman::p_load(httr,
+               dplyr,
+               tidyr)
 
-
-getUrl <- function(year, plan, country) {
+#' getUrl
+#' 
+#' @year The year boundary
+#' @planid The plan boundary
+#' 
+#' @description Construct FTS API URL
+#' 
+#' Could be expanded to include more parameters - parameters should be named according
+#' to API spec (default values and null checks would be necessary)
+#'
+getUrl <- function(year, planid) {
   flowsUrl <- "https://api.hpc.tools/v1/public/fts/flow"
-  arguments <- as.list(match.call())
-  arguments[1] <- NULL # remove first item (function name)
-  queryParams <- paste(lapply(names(arguments), function(n) { paste(n, arguments[n], sep="=") } ), collapse="&")
+  arguments <- as.list(environment())[-1] # get function call and remove first item (function name)
+  arguments["format"] = "json"
+  queryParams <- paste(lapply(names(arguments), function(n) { 
+    val <- arguments[n]
+    paste(n, val, sep="=") 
+  } ), collapse="&")
   url <- paste(flowsUrl, queryParams, sep="?")
   url
 }
 
-getFlows <- function(year, plan, country) {
+#' getFlowsRecursive
+#' 
+#' @param df A data frame (for recursive accumulation). When provided, the results
+#' of the API calls are combined into the data frame (rbind) 
+#' @param url A fully qualified URL for retrieving FTS data. If null, the data frame
+#' is returned
+#' 
+#' @description Retrieve all flows starting from the provided URL using the "next" link returned by
+#' each subsequent request, until no next URL is available
+#' 
+getFlowsRecursive <- function(df=NULL, url) {
+  if (!is.null(url)) {
+    print(sprintf('Retrieving %s', url))
+    
+    # get data page
+    res = httr::GET(url)
+    
+    # stop if error
+    httr::stop_for_status(res, paste("get",url))
+    
+    # convert json to object
+    resData <- jsonlite::fromJSON(httr::content(res, "text"))
+    
+    # append flows to data frame
+    if (is.null(df)) 
+      df <- resData$data$flows
+    else
+      df <- rbind(df, resData$data$flows)
+
+    # get next data page
+    getFlowsRecursive(df, resData$meta$nextLink)
+  }
+  df
+}
+
+#' getFlows
+#' @param year Required, the boundary year
+#' @param planid Required, the boundary plan id
+#' 
+#' Returns all flows on the boundary from FTS
+#' 
+getFlows <- function(year, planid) {
+  print(sprintf("Getting flows for year=%s, plan=%s", year, planid))
+  url <- getUrl(year, planid)
+  getFlowsRecursive(url=url)
 }
